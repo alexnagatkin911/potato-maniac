6cadbaf9434f5172ba6fce764d06dcb05309018d
Kris Chery <kris.chery2@gmail.com>
2017-09-25 00:21:25 +0000

    second - commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+ 
+ resources :products
+ resources :order_items
+ resource :carts, only: [:show]
+ resources :charges
+ root "home#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
